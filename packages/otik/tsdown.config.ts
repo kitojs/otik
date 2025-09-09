0aab5b3ce6702cc870af2001840650c7a4cca25a
nehu3n <developer.nh3@gmail.com>
2025-09-09 18:45:38 -0300

    build(otik): add tsdown

---
+++ packages/otik/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: "src/index.ts",
+  outDir: "dist",
+
+  format: "esm",
+  minify: true,
+
+  dts: true,
+  tsconfig: "tsconfig.json",
+});
